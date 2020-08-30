e58f0c057fbe507b74595d574c8c550ac2bcd9ad
Darwin Yuan <darwin_yuan@hotmail.com>
2020-08-30 23:17:47 +0800

    added repeat_call2

--- include/maco/aggregate.h
+++ include/maco/aggregate.h
@@ -6,13 +6,9 @@
 #define MACO_AGGREGATE_H
 
 #include <maco/basic.h>
-#include <maco/pp_size.h>
 
 #define __MACO_var_full(x) __MACO_swap_ x
 #define __MACO_var_name(x) __MACO_1st   x
 #define __MACO_var_type(x) __MACO_rest  x
-
-#define __MACO_foreach(macro, ...) \
-__MACO_overload(__MACO_repeat_call_, __VA_ARGS__) (macro, 0, __VA_ARGS__)
 
 #endif //MACO_AGGREGATE_H
