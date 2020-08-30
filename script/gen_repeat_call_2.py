5691bb99aac2ed3aa4a8b580e6405d3f9b2cb274
Darwin Yuan <darwin_yuan@hotmail.com>
2020-08-30 22:58:50 +0800

    added repeat_call2

---
+++ script/gen_repeat_call_2.py
@@ -0,0 +1,36 @@
+import sys
+
+head = '''
+#ifndef __MACO_MACRO_REPEAT_CALL_2_H_
+#define __MACO_MACRO_REPEAT_CALL_2_H_
+
+#include <maco/int_succ.h>
+
+'''
+
+def gen_repeat_call_macro(n):
+    all_macros = ""
+    for i in range(n):
+        all_macros = all_macros + "#define __MACO_repeat_call_2_{}(call, i, x, ...)".format(i)
+        if i > 0:
+            all_macros = all_macros + " call(i, x) __MACO_repeat_call_2_{}(call, __MACO_succ(i), __VA_ARGS__)".format(i - 1)
+
+        all_macros = all_macros + "\n"
+
+    return all_macros
+
+def main():
+    # print command line arguments
+    if len(sys.argv) != 3:
+        print("specify the num and file")
+        exit(-1)
+
+    n = int(sys.argv[1]) + 1
+    lines = head +  gen_repeat_call_macro(n) + "\n\n#endif"
+
+    file = open(sys.argv[2], "w")
+    file.writelines(lines)
+    file.close()
+
+if __name__ == "__main__":
+    main()
