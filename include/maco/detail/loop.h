79039437b5e4e3d3f0a04bcab9d0e00eec0ced59
Darwin Yuan <darwin_yuan@hotmail.com>
2020-09-03 02:37:54 +0800

    added loop

--- include/maco/detail/loop.h
+++ include/maco/detail/loop.h
@@ -5,13 +5,12 @@
 #ifndef MACO_LOOP_DETAIL_H
 #define MACO_LOOP_DETAIL_H
 
-//#define __MACO_eval_4(...)  __MACO_eval_5(__MACO_eval_5(__MACO_eval_5(__VA_ARGS__)))
-//#define __MACO_eval_5(...)  __MACO_eval_6(__MACO_eval_6(__MACO_eval_6(__VA_ARGS__)))
-//#define __MACO_eval_6(...)  __MACO_eval_7(__MACO_eval_7(__MACO_eval_7(__VA_ARGS__)))
-
-#define __MACO_eval_2(...)  __VA_ARGS__
-//#define __MACO_eval_3(...)  __MACO_eval_4(__MACO_eval_4(__MACO_eval_4(__VA_ARGS__)))
-//#define __MACO_eval_2(...)  __MACO_eval_3(__MACO_eval_3(__MACO_eval_3(__VA_ARGS__)))
+#define __MACO_eval_7(...)  __VA_ARGS__
+#define __MACO_eval_6(...)  __MACO_eval_7(__MACO_eval_7(__MACO_eval_7(__VA_ARGS__)))
+#define __MACO_eval_5(...)  __MACO_eval_6(__MACO_eval_6(__MACO_eval_6(__VA_ARGS__)))
+#define __MACO_eval_4(...)  __MACO_eval_5(__MACO_eval_5(__MACO_eval_5(__VA_ARGS__)))
+#define __MACO_eval_3(...)  __MACO_eval_4(__MACO_eval_4(__MACO_eval_4(__VA_ARGS__)))
+#define __MACO_eval_2(...)  __MACO_eval_3(__MACO_eval_3(__MACO_eval_3(__VA_ARGS__)))
 #define __MACO_eval_1(...)  __MACO_eval_2(__MACO_eval_2(__MACO_eval_2(__VA_ARGS__)))
 #define __MACO_eval_(...)   __MACO_eval_1(__MACO_eval_1(__MACO_eval_1(__VA_ARGS__)))
 
