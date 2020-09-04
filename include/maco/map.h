f424675f8363ee29d4ddd5a4a471ada0967bb7da
Darwin Yuan <darwin_yuan@hotmail.com>
2020-09-04 08:57:11 +0800

    added maco

--- include/maco/map.h
+++ include/maco/map.h
@@ -24,7 +24,7 @@ __MACO_overload(__MACO_repeat_call_, __VA_ARGS__) (f, 0, __VA_ARGS__)
 #define __MACO_map_core_(next, f, m, n, x, ...)   f(m, x) __MACO_while(n)(next, (f, __MACO_inc(m), __MACO_dec(n), __VA_ARGS__))
 #define __MACO_map_2__(f, m, n, x, ...)           __MACO_map_core_(__MACO_map_1__, f, m, n, x, __VA_ARGS__)
 #define __MACO_map_1__(f, m, n, x, ...)           __MACO_map_core_(__MACO_map_2__, f, m, n, x, __VA_ARGS__)
-#define __MACO_map_(f, n, ...)                    __MACO_loop(n, __MACO_map_1__(f, 0, __MACO_prev(n), __VA_ARGS__))
+#define __MACO_map_(f, n, ...)                    __MACO_loop(n, __MACO_map_1__(f, 0, __MACO_dec(n), __VA_ARGS__))
 #define __MACO_map(f, ...)                        __MACO_map_(f, __MACO_num_of_args(__VA_ARGS__), __VA_ARGS__)
 
 #endif
