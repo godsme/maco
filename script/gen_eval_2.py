eb81816a1f83b7c15e67fd040eac72188576bd14
Darwin Yuan <darwin_yuan@hotmail.com>
2020-09-04 08:22:19 +0800

    added eval

---
+++ script/gen_eval_2.py
@@ -0,0 +1,38 @@
+import sys
+
+head = '''
+#ifndef __MACO_INTERNAL_EVAL_2__
+#define __MACO_INTERNAL_EVAL_2__
+
+#define __MACO_eval_2_0(...) 
+#define __MACO_eval_2_1(...) __VA_ARGS__
+'''
+
+end = '''
+
+#endif
+'''
+
+def gen_eval(n):
+    seq = ""
+    for i in range(n):
+        seq = seq + "#define __MACO_eval_2_{}(...)   __MACO_eval_2_{}(__VA_ARGS__)\n".format(i+2, i+1)
+
+    return seq
+
+def main():
+    # print command line arguments
+    if len(sys.argv) != 3:
+        print("specify the num and file")
+        exit(-1)
+
+    n = int(sys.argv[1])
+    lines = head + gen_eval(n) + end
+    print(lines)
+
+    file = open(sys.argv[2], "w")
+    file.writelines(lines)
+    file.close()
+
+if __name__ == "__main__":
+    main()
